1a18e5e0c7ecdbb3da0defaec40c19d6e1ab9d36
Anmol Verma <aanmolverma1309@gmail.com>
2026-07-24 18:33:17 +0530

    Time: 0 ms (100%), Space: 61.1 MB (71.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -16,38 +16,4 @@ class Solution {
         reverse(nums, k, n-1);
     }
 
-    //     int arr[] = new int [nums.length];
-    //     int j =0;
-    //     for (int i =nums.length-1; i >= 0; i--){
-    //         arr[j] = nums[i];
-    //         j++;
-    //     }
-    //     int lp = 0;
-    //     int rp = k-1;
-
-    //     for (int i = lp; i<= rp; i++){
-    //         int temp = arr[lp];
-    //         arr[lp] = arr[rp];
-    //         arr[rp] = temp;
-    //         lp++;
-    //         rp--;
-
-    //     }
-    //     int first = k;
-    //     int last = nums.length-1;
-    //     for (int i = k; i<= last; i++ ){
-    //         int temp = arr[first];
-    //         arr[first] = arr[last];
-    //         arr[last] = temp;
-    //         first++;
-    //         last--;
-    //     }
-
-    //     for(int i = 0; i< nums.length; i++){
-    //         nums[i] = arr[i];
-    //     }
-       
-
-        
-    // }
 }
