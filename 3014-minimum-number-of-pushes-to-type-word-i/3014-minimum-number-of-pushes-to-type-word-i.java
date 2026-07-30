34b88e45579657d81d3c6cf4bcb6bd9f5976b419
Anmol Verma <aanmolverma1309@gmail.com>
2026-07-30 22:44:46 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.51%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int cost = 0;
+        for(int i = 0; i< word.length(); i++){
+            cost+= (i/8) +1;
+        }
+        return cost;
+        
+    }
+}
