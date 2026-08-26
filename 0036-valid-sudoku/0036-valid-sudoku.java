ba1ad9d088f709ff609f0460f6adf482dac41c4e
Anmol Verma <aanmolverma1309@gmail.com>
2026-08-26 22:48:15 +0530

    Time: 3 ms (50.3%), Space: 46.5 MB (55.22%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,45 +1,69 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] columns = new HashSet[9];
         HashSet<Character>[] boxes = new HashSet[9];
         for(int i = 0; i < 9; i++){
             boxes[i] = new HashSet<>();
+            rows[i] = new HashSet<>();
+            columns[i] = new HashSet<>();
         }
         for(int i = 0; i< board.length; i++){
-            HashSet<Character> row = new HashSet<>();
-            for(int j = 0; j< board[i].length; j++){
-                if(board[i][j] == '.') {
+            for(int  j = 0; j< board[i].length;j++){
+                char nums = board[i][j];
+                int boxid = (i/3)*3 +j/3;
+                if(nums == '.'){
                     continue;
                 }
-                if(row.contains(board[i][j])){
-                    return false;
-                }else{
-                    row.add(board[i][j]);
-                }
-            }
-        }for(int i = 0; i < board.length; i++){
-            HashSet<Character> column = new HashSet<>();
-            for(int j = 0; j < board[i].length; j++){
-                if(board[j][i] == '.'){
-                    continue;
-                }
-                if(column.contains(board[j][i])){
+                if(rows[i].contains(nums)|| columns[j].contains(nums)|| boxes[boxid].contains(nums)){
                     return false;
                 }
-                column.add(board[j][i]);
+                rows[i].add(nums);
+                columns[j].add(nums);
+                boxes[boxid].add(nums);
+
             }
-        }
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if(board[i][j] == '.'){
-                    continue;
-                }
-                int boxid = (i / 3) * 3 + j / 3;
-                if(boxes[boxid].contains(board[i][j])){
-                    return false;
-                }
-                boxes[boxid].add(board[i][j]);
-            }
-        }
-        return true;
+        }return true;
+        // HashSet<Character>[] boxes = new HashSet[9];
+        // for(int i = 0; i < 9; i++){
+        //     boxes[i] = new HashSet<>();
+        // }
+        // for(int i = 0; i< board.length; i++){
+        //     HashSet<Character> row = new HashSet<>();
+        //     for(int j = 0; j< board[i].length; j++){
+        //         if(board[i][j] == '.') {
+        //             continue;
+        //         }
+        //         if(row.contains(board[i][j])){
+        //             return false;
+        //         }else{
+        //             row.add(board[i][j]);
+        //         }
+        //     }
+        // }for(int i = 0; i < board.length; i++){
+        //     HashSet<Character> column = new HashSet<>();
+        //     for(int j = 0; j < board[i].length; j++){
+        //         if(board[j][i] == '.'){
+        //             continue;
+        //         }
+        //         if(column.contains(board[j][i])){
+        //             return false;
+        //         }
+        //         column.add(board[j][i]);
+        //     }
+        // }
+        // for(int i = 0; i < board.length; i++){
+        //     for(int j = 0; j < board[i].length; j++){
+        //         if(board[i][j] == '.'){
+        //             continue;
+        //         }
+        //         int boxid = (i / 3) * 3 + j / 3;
+        //         if(boxes[boxid].contains(board[i][j])){
+        //             return false;
+        //         }
+        //         boxes[boxid].add(board[i][j]);
+        //     }
+        // }
+        // return true;
     }
 }
