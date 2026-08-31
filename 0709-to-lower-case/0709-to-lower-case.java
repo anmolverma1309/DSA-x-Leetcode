1d2025f99dadec931d07185258e84aa2f92190a9
Anmol Verma <aanmolverma1309@gmail.com>
2026-08-31 23:11:24 +0530

    Time: 1 ms (25.26%), Space: 42.9 MB (68.01%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,12 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char[] arr = s.toCharArray();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) >= 65 && s.charAt(i) <= 90){
+                arr[i] += 32;
+            }
+        }
+        return new String(arr);
+        // return s.toLowerCase();
     }
 }
