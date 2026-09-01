16fd1ef5245c3f6afbb5c36fcc147a4ec0125d52
Anmol Verma <aanmolverma1309@gmail.com>
2026-09-01 18:37:22 +0530

    Time: 1 ms (96.08%), Space: 42.9 MB (89.34%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,26 +1,41 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int idx2 = word2.length();
-        int idx1 = word1.length();
-        int smallest = Integer.MAX_VALUE;
-        if(idx1 < idx2){
-            smallest = idx1;
-        }else{
-            smallest = idx2;
-        }
+        int i = 0; 
+        int j = 0;
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<smallest; i++){
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
+        while(i<word1.length() && j<word2.length()){
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
         }
-        while(smallest< word1.length()){
-            sb.append(word1.charAt(smallest));
-            smallest++;
+        while(i<word1.length()){
+            sb.append(word1.charAt(i++));
         }
-        while(smallest< word2.length()){
-            sb.append(word2.charAt(smallest));
-            smallest++;
-        }
-        return sb.toString();
+        while(j<word2.length()){
+            sb.append(word2.charAt(j++));
+        }return sb.toString();
+
+
+        // int idx2 = word2.length();
+        // int idx1 = word1.length();
+        // int smallest = Integer.MAX_VALUE;
+        // if(idx1 < idx2){
+        //     smallest = idx1;
+        // }else{
+        //     smallest = idx2;
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0; i<smallest; i++){
+        //     sb.append(word1.charAt(i));
+        //     sb.append(word2.charAt(i));
+        // }
+        // while(smallest< word1.length()){
+        //     sb.append(word1.charAt(smallest));
+        //     smallest++;
+        // }
+        // while(smallest< word2.length()){
+        //     sb.append(word2.charAt(smallest));
+        //     smallest++;
+        // }
+        // return sb.toString();
     }
 }
