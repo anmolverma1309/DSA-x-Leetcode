0fcbadd198329842f7be6665dfa11035745a1a79
Anmol Verma <aanmolverma1309@gmail.com>
2026-09-03 23:34:52 +0530

    Time: 2 ms (100%), Space: 121.9 MB (30.44%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,23 +1,18 @@
 class Solution {
-    
-    private boolean check(int parity, int[] nums1, int minodd){
-        for(int i = 0 ; i< nums1.length; i++){
-            if((nums1[i] & 1) != parity && nums1[i] <= minodd ){
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i< nums1.length; i++){
+            if(nums1[i] < min){
+                min = nums1[i];
+            }
+        }
+        if(min % 2 == 1){
+            return true;
+        }
+        for(int i = 0; i< nums1.length;i++){
+            if(nums1[i]%2 == 1){
                 return false;
             }
-        }
-        return true;
-    }
-    public boolean uniformArray(int[] nums1) {
-        int minodd = Integer.MAX_VALUE;
-        for(int i = 0; i< nums1.length; i++){
-            if(nums1[i]%2 != 0 && nums1[i] < minodd){
-                minodd = nums1[i];
-            }
-        }
-        boolean even = check(0, nums1, minodd);
-        boolean odd = check(1,nums1, minodd);
-        
-        return even|| odd;
+        }return true;
     }
 }
