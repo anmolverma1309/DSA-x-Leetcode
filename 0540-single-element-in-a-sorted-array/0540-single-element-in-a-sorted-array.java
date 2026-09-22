7bf71823596e6a51a578bfaac8dfa3fcc0415285
Anmol Verma <aanmolverma1309@gmail.com>
2026-09-22 22:34:12 +0530

    Time: 0 ms (100%), Space: 52.6 MB (89.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,13 +3,19 @@ class Solution {
         int s = 0, e = nums.length-1;
         while(s< e){
             int mid = s+(e-s)/2;
-            if(mid%2 == 1){
-                mid--;
-            }
-            if(nums[mid] != nums[mid+1]){
-                e = mid;
-            }else{
-                s = mid+2;
+            if(mid%2 == 0){
+                if(nums[mid] == nums[mid+1]){
+                    s = mid+2;
+                }else{
+                    e = mid;
+                }
+            } 
+            else{
+                if(nums[mid] == nums[mid-1]){
+                    s = mid+1;
+                }else{
+                    e = mid-1;
+                }
             }
         }return nums[s];
     }
